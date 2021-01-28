9576cf3ba3ebcfd7bbf4a3e16292bedbc8dc9775
TischerT <72385455+TischerT@users.noreply.github.com>
2021-01-28 11:19:22 +0000

    Update clicker.py
    Move find your own values to separate file.
    Potential fix for [Issue 1](https://github.com/TischerT/Brave_rewards_clicker/issues/1)

--- clicker.py
+++ clicker.py
@@ -13,34 +13,25 @@ brave_close_x = 1725
 brave_close_y = 995
 # postion of an orange pixel of the brave icon in the notification area
 brave_pix_x = 1561
-brave_pix_y = 931
+brave_pix_y_1 = 931
+brave_pix_y_2 = 911
 # how often the script is run in seconds
 sleep_timer = 5
 # pixel value to compare in RGB, default (255, 85, 0) is orange
 pixel_compare_val = (255, 85, 0)
 
-# find your own values
-# uncomment and move the mouse on top of the brave icon in the notification area
-# try to use an orange pixel
-# run script and note down the values
-# first value is position x and y, second value is RGB value of the pixel
-# for brave_close_x and y add about 200 to the position values
-# change values above accordingly
-
-# currentp = mouse.position
-# print(currentp)
-# im = pyautogui.screenshot()
-# pix = im.getpixel(currentp)
-# print(pix)
-
 
 def check_pixel_val():
     try:
-        pix_val = pyautogui.pixel(brave_pix_x, brave_pix_y)
+        pix_val = pyautogui.pixel(brave_pix_x, brave_pix_y_1)
+        pix_val_2 = pyautogui.pixel(brave_pix_x, brave_pix_y_2)
+        print(pix_val + pix_val_2)
     except:
-        #       print("Cannot get pixel for the moment")
+        print("Cannot get pixel for the moment")
         pix_val = (0, 0, 0)
-    if pix_val == pixel_compare_val:
+        pix_val_2 = (0, 0, 0)
+    # if pix_val == pixel_compare_val:
+    if pix_val == pixel_compare_val or pix_val_2 == pixel_compare_val:
         return True
     else:
         return False
